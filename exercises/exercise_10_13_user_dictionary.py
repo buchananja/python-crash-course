b69f5387c8bb4d713271267aa38699a8bf2ef3c4
James Buchanan <james42buchanan@gmail.com>
2024-01-26 20:36:06 +0000

    Finished chapter 10.

--- exercises/exercise_10_13_user_dictionary.py
+++ exercises/exercise_10_13_user_dictionary.py
@@ -11,6 +11,7 @@ def get_stored_account(path):
         return None
             
 def get_new_account(path):
+    # Setting account details
     user_details = dict()
     username = input('Please enter a username: ')
     user_details.update({'username': username})
@@ -18,19 +19,36 @@ def get_new_account(path):
     user_details.update({'mobile': mobile})
     email = input('Please enter an email address: ')
     user_details.update({'email': email})
+    # Setting password
+    user_password = dict
+    password = input('Please enter your password: ')
+    user_password.update({'password': password})
 
     contents = json.dumps(user_details)
     path.write_text(contents)
     print('Account created successfully!')
-        
+
+def check_password(path):
+    '''Retrives stored username if present.'''
+    if path.exists():
+        contents = path.read_text()
+        user_password = json.loads(contents)
+        return user_password
+    else: 
+        return None
+
 def greet_user():
     '''Greet user by username and display details.'''
     path = Path('user_details.json')
     user_details = get_stored_account(path)
 
     if user_details:
-        # print(type(user_details))
-        {print(f'{k.title()}: {v}') for k, v in user_details.items()}
+        print(f'Remembered username: {user_details["username"]}')
+        account_response = input(f'Is this your username? (yes/no): ')
+        if account_response.lower() == 'yes':
+                {print(f'{k.title()}: {v}') for k, v in user_details.items()}
+        else:
+            print('Exiting...') 
     else:
         account_response = input(
             'Account does not exist, would you like to create one? (yes/no): '
